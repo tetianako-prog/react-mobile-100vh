c167b41b115b3d2ea94ddb9136356ca5572777d0
tetianako-prog <t.kobyliakova@gmail.com>
2021-12-13 21:10:31 +0200

    Updates

---
+++ static/js/main.eec21ca8.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-mobile-100vh"]=this["webpackJsonpreact-mobile-100vh"]||[]).push([[0],{10:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),i=c(3),o=c.n(i),s=(c(8),c(0));var h=function(){return Object(s.jsx)("div",{children:Object(s.jsx)("h1",{children:"React 100vh"})})};o.a.render(Object(s.jsx)(r.a.StrictMode,{children:Object(s.jsx)(h,{})}),document.getElementById("root"))},8:function(e,t,c){}},[[10,1,2]]]);
+//# sourceMappingURL=main.eec21ca8.chunk.js.map
